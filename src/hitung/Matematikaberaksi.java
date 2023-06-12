52380b71c0ceba601ec3c203a7d516676f78ad94
bale0607 <iqbal.np20@gmail.com>
2023-06-12 05:14:15 -0700

    Membuat class MatematikaBeraksi

--- src/hitung/Matematikaberaksi.java
+++ src/hitung/Matematikaberaksi.java
@@ -2,6 +2,13 @@
 package hitung;
 
 public class Matematikaberaksi {
-    
+    public static void main(String[] args) {
+        Matematika iqbal = new Matematika(8,3);
+        
+        System.out.println("Hasil penjumlahan: "+iqbal.setPenjumlahan());
+        System.out.println("Hasil pengurangan: "+iqbal.setPengurangan());
+        System.out.println("Hasil perkalian: "+iqbal.setPerkalian());
+        System.out.println("Hasil pembagian: "+iqbal.setPembagian());
     }
+ }
 
